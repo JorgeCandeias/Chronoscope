8c263d64f3a164b122139ae50a193530c4bb07a5
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-19 09:44:40 +0100

    Added tracking activity interface

---
+++ src/Chronoscope.Core.Abstractions/ITrackingActivity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Chronoscope
+{
+    /// <summary>
+    /// Defines a tracking activity as created by a scope.
+    /// </summary>
+    [SuppressMessage("Naming", "CA1716:Identifiers should not match keywords")]
+    public interface ITrackingActivity
+    {
+        Guid Id { get; }
+
+        void Start();
+
+        void Stop();
+    }
+}
