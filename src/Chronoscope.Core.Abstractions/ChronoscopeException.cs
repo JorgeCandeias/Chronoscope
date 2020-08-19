7788cfc7edc51735a6ea810b0b6627235b8f60a4
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-19 09:35:22 +0100

    Added exception

---
+++ src/Chronoscope.Core.Abstractions/ChronoscopeException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Chronoscope
+{
+    [Serializable]
+    public class ChronoscopeException : Exception
+    {
+        public ChronoscopeException()
+        {
+        }
+
+        public ChronoscopeException(string message) : base(message)
+        {
+        }
+
+        public ChronoscopeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        #region Serialization
+
+        protected ChronoscopeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        #endregion Serialization
+    }
+}
