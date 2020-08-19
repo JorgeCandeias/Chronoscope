67b0a76bf176711874f65d7d6fe3c1527c884011
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-19 08:17:37 +0100

    Added builder interface

---
+++ src/Chronoscope.Core.Abstractions/IChronoscopeBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Chronoscope
+{
+    /// <summary>
+    /// Allows adding chronoscope specific services to the underlying host builder.
+    /// </summary>
+    public interface IChronoscopeBuilder
+    {
+        /// <summary>
+        /// Allows adding services to the underlying host.
+        /// </summary>
+        /// <param name="configure">The callback to use for adding services.</param>
+        /// <returns>The same builder instance to allow chaining.</returns>
+        public IChronoscopeBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configure);
+    }
+
+    /// <summary>
+    /// Quality-of-life extension methods for <see cref="IChronoscopeBuilder"/>.
+    /// </summary>
+    public static class ChronoscopeBuilderExtensions
+    {
+        /// <summary>
+        /// Allows adding services to the underlying host.
+        /// </summary>
+        /// <param name="builder">The builder to extend.</param>
+        /// <param name="configure">The callback to use for adding services.</param>
+        /// <returns>The same builder instance to allow chaining.</returns>
+        public static IChronoscopeBuilder ConfigureServices(this IChronoscopeBuilder builder, Action<IServiceCollection> configure)
+        {
+            if (builder is null) throw new ArgumentNullException(nameof(builder));
+            if (configure is null) throw new ArgumentNullException(nameof(configure));
+
+            return builder.ConfigureServices((context, services) => configure(services));
+        }
+    }
+}
--- src/Chronoscope.Core/TaskScope.cs
+++ src/Chronoscope.Core/TaskScope.cs
@@ -7,26 +7,24 @@ namespace Chronoscope
 {
     internal class TaskScope : ITaskScope
     {
-        private readonly IOptions<ChronoScopeOptions> _options;
         private readonly ILogger<TaskScope> _logger;
         private readonly ITaskScopeFactory _factory;
 
-        private readonly Guid? _parentId;
-
         internal TaskScope(IOptions<ChronoScopeOptions> options, ILogger<TaskScope> logger, ITaskScopeFactory factory, Guid id, string? name, Guid? parentId)
         {
-            _options = options;
             _logger = logger;
             _factory = factory;
 
             Id = id;
             Name = name ?? string.Format(CultureInfo.InvariantCulture, options.Value.DefaultTaskScopeNameFormat, id);
-            _parentId = parentId;
+            ParentId = parentId;
         }
 
         public Guid Id { get; }
 
         public string Name { get; }
+
+        public Guid? ParentId { get; }
 
         public ITaskScope CreateScope(Guid id, string? name) => _factory.CreateScope(id, name, Id);
     }
