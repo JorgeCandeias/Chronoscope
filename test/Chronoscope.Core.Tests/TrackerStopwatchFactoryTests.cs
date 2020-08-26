a11988ee1ea108385b0aecc7a74adcc787751f14
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-27 00:56:31 +0100

    Added tests

---
+++ test/Chronoscope.Core.Tests/TrackerStopwatchFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Chronoscope.Core.Tests
+{
+    public class TrackerStopwatchFactoryTests
+    {
+        [Fact]
+        public void CreateReturnsNewInstance()
+        {
+            // arrange
+            var factory = new TrackerStopwatchFactory();
+
+            // act
+            var result1 = factory.Create();
+            var result2 = factory.Create();
+
+            // assert
+            Assert.NotNull(result1);
+            Assert.NotNull(result2);
+            Assert.NotSame(result1, result2);
+        }
+    }
+}
