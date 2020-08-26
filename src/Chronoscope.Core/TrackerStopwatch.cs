432fe17b682a646f10cc344f840d6b286b535d2a
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-27 00:51:29 +0100

    Refactored

--- src/Chronoscope.Core/TrackerStopwatch.cs
+++ src/Chronoscope.Core/TrackerStopwatch.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace Chronoscope
 {
     /// <summary>
     /// Default implementation of <see cref="ITrackerStopwatch"/>.
     /// </summary>
-    public class TrackerStopwatch : ITrackerStopwatch
+    public class TrackerStopwatch : Stopwatch, ITrackerStopwatch
     {
-        private readonly Stopwatch _watch = new Stopwatch();
-
-        public TimeSpan Elapsed => _watch.Elapsed;
-
-        public void Start() => _watch.Start();
-
-        public void Stop() => _watch.Stop();
     }
 }
