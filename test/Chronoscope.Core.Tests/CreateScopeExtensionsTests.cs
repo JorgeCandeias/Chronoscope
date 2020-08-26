2b32adda4fb32d24227735d5404b77287e0fa48b
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-27 00:47:58 +0100

    Added tests

---
+++ test/Chronoscope.Core.Tests/CreateScopeExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+namespace Chronoscope.Core.Tests
+{
+    public class CreateScopeExtensionsTests
+    {
+        [Fact]
+        public void CreateScopeWithNameReturnsNewScope()
+        {
+            // arrange
+            var name = Guid.NewGuid().ToString();
+            var scope = Mock.Of<ITrackingScope>();
+            var creator = Mock.Of<ICreateScope>(x => x.CreateScope(It.IsNotIn(Guid.Empty), name) == scope);
+
+            // act
+            var result = creator.CreateScope(name);
+
+            // assert
+            Assert.Same(scope, result);
+            Mock.Get(creator).VerifyAll();
+        }
+
+        [Fact]
+        public void CreateScopeWithoutParamsReturnsNewScope()
+        {
+            // arrange
+            var scope = Mock.Of<ITrackingScope>();
+            var creator = Mock.Of<ICreateScope>(x => x.CreateScope(It.IsNotIn(Guid.Empty), null) == scope);
+
+            // act
+            var result = creator.CreateScope();
+
+            // assert
+            Assert.Same(scope, result);
+            Mock.Get(creator).VerifyAll();
+        }
+    }
+}
