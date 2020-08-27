3cd7bfd7506403848bd7e114bc48b76dec12c272
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-27 01:13:29 +0100

    Cleanup

--- test/Chronoscope.Core.Tests/ChronoscopeExceptionTests.cs
+++ test/Chronoscope.Core.Tests/ChronoscopeExceptionTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml;
-using System.Xml.Serialization;
 using Xunit;
 
 namespace Chronoscope.Core.Tests
