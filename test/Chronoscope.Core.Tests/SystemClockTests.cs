29271d0b1d6693f58829f07aaf09d856cde9b041
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-30 00:57:43 +0100

    Added test

---
+++ test/Chronoscope.Core.Tests/SystemClockTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace Chronoscope.Core.Tests
+{
+    public class SystemClockTests
+    {
+        [Fact]
+        public void NowIsNow()
+        {
+            // arrange
+            var clock = new SystemClock();
+            var now = DateTimeOffset.Now;
+
+            // act
+            var result = clock.Now;
+
+            // assert
+            Assert.True(result > now.AddMilliseconds(-1));
+            Assert.True(result < now.AddMilliseconds(1));
+        }
+    }
+}
