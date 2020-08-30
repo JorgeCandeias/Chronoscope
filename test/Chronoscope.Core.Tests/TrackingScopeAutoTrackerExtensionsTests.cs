4a57e0f152a4415a289db3771477c0d6804514e4
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-31 00:12:17 +0100

    Added tests

---
+++ test/Chronoscope.Core.Tests/TrackingScopeAutoTrackerExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace Chronoscope.Core.Tests
+{
+    public class TrackingScopeAutoTrackerExtensionsTests
+    {
+        [Fact]
+        public void TrackWithIdAndScopeAndTokenThrowsOnNullScope()
+        {
+            // arrange
+            ITrackingScope scope = null;
+            var id = Guid.NewGuid();
+            Action<ITrackingScope, CancellationToken> workload = null;
+            var token = CancellationToken.None;
+
+            // act
+            var ex = Assert.Throws<ArgumentNullException>(() => scope.Track(id, workload, token));
+
+            // assert
+            Assert.Equal(nameof(scope), ex.ParamName);
+        }
+
+        [Fact]
+        public void TrackWithIdAndScopeAndTokenWorks()
+        {
+            // arrange
+            var id = Guid.NewGuid();
+            var tracker = Mock.Of<IAutoTracker>();
+            ITrackingScope scope = Mock.Of<ITrackingScope>(x => x.CreateAutoTracker(id) == tracker);
+            Action<ITrackingScope, CancellationToken> workload = null;
+            var token = CancellationToken.None;
+
+            // act
+            scope.Track(id, workload, token);
+
+            // assert
+            Mock.Get(scope).VerifyAll();
+            Mock.Get(tracker).Verify(x => x.Track(workload, token));
+        }
+    }
+}
