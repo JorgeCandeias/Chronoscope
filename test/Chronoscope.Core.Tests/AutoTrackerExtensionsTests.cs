746ae053993a6a4d2929e5d1757a4b56636997ad
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-30 18:23:09 +0100

    Added tests

--- test/Chronoscope.Core.Tests/AutoTrackerExtensionsTests.cs
+++ test/Chronoscope.Core.Tests/AutoTrackerExtensionsTests.cs
@@ -40,9 +40,53 @@ namespace Chronoscope.Core.Tests
             // arrange
             var tracker = new FakeAutoTracker();
             var called = false;
+            void workload(ITrackingScope scope) { called = true; }
 
             // act
-            tracker.Track(scope => { called = true; });
+            tracker.Track(workload);
+
+            // assert
+            Assert.True(called);
+        }
+
+        [Fact]
+        public void TrackWorkloadThrowsOnNullTracker()
+        {
+            // arrange
+            IAutoTracker tracker = null;
+            Action workload = null;
+
+            // act
+            var ex = Assert.Throws<ArgumentNullException>(() => tracker.Track(workload));
+
+            // assert
+            Assert.Equal(nameof(tracker), ex.ParamName);
+        }
+
+        [Fact]
+        public void TrackWorkloadThrowsOnNullWorkload()
+        {
+            // arrange
+            var tracker = new FakeAutoTracker();
+            Action workload = null;
+
+            // act
+            var ex = Assert.Throws<ArgumentNullException>(() => tracker.Track(workload));
+
+            // assert
+            Assert.Equal(nameof(workload), ex.ParamName);
+        }
+
+        [Fact]
+        public void TrackWorkloadWorks()
+        {
+            // arrange
+            var tracker = new FakeAutoTracker();
+            var called = false;
+            void workload() { called = true; }
+
+            // act
+            tracker.Track(workload);
 
             // assert
             Assert.True(called);
