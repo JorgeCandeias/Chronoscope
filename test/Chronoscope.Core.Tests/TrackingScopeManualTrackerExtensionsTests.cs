6244cff4d55ec49c86bfe9c6d5b4c862a80dfadb
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-30 21:23:44 +0100

    Added tests

---
+++ test/Chronoscope.Core.Tests/TrackingScopeManualTrackerExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+namespace Chronoscope.Core.Tests
+{
+    public class TrackingScopeManualTrackerExtensionsTests
+    {
+        [Fact]
+        public void CreateManualTrackerThrowsOnNullScope()
+        {
+            // arrange
+            ITrackingScope scope = null;
+
+            // act
+            var ex = Assert.Throws<ArgumentNullException>(() => scope.CreateManualTracker());
+
+            // assert
+            Assert.Equal(nameof(scope), ex.ParamName);
+        }
+
+        [Fact]
+        public void CreateManualTrackerWorks()
+        {
+            // arrange
+            var tracker = Mock.Of<IManualTracker>();
+            var scope = Mock.Of<ITrackingScope>(x => x.CreateManualTracker(It.IsAny<Guid>()) == tracker);
+
+            // act
+            var result = scope.CreateManualTracker();
+
+            // assert
+            Mock.Get(scope).VerifyAll();
+            Assert.Same(tracker, result);
+        }
+
+        [Fact]
+        public void StartManualTrackerThrowsOnNullScope()
+        {
+            // arrange
+            ITrackingScope scope = null;
+            var id = Guid.NewGuid();
+
+            // act
+            var ex = Assert.Throws<ArgumentNullException>(() => scope.StartManualTracker(id));
+
+            // assert
+            Assert.Equal(nameof(scope), ex.ParamName);
+        }
+
+        [Fact]
+        public void StartManualTrackerWorks()
+        {
+            // arrange
+            var id = Guid.NewGuid();
+            var tracker = Mock.Of<IManualTracker>();
+            var scope = Mock.Of<ITrackingScope>(x => x.CreateManualTracker(id) == tracker);
+
+            // act
+            var result = scope.StartManualTracker(id);
+
+            // assert
+            Mock.Get(scope).VerifyAll();
+            Mock.Get(tracker).Verify(x => x.Start());
+            Assert.Same(tracker, result);
+        }
+    }
+}
