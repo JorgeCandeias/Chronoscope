a154e7a06394ce9325b06d315921134f07b2b9a5
Jorge Candeias <jorge.candeias@outcompute.com>
2020-08-30 01:16:18 +0100

    Added test

---
+++ test/Chronoscope.Core.Tests/TrackingScopeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using Xunit;
+
+namespace Chronoscope.Core.Tests
+{
+    public class TrackingScopeTests
+    {
+        [Fact]
+        public void ThrowsOnNullContext()
+        {
+            // arrange
+            var options = Options.Create(new ChronoscopeOptions());
+            IChronoscopeContext? context = null;
+            var id = Guid.NewGuid();
+            var name = Guid.NewGuid().ToString();
+            var parentId = Guid.NewGuid();
+
+            // act
+            var result = Assert.Throws<ArgumentNullException>(() => new TrackingScope(options, context, id, name, parentId));
+
+            // assert
+            Assert.Equal(nameof(context), result.ParamName);
+        }
+    }
+}
